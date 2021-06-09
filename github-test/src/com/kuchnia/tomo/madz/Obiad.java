17d734a3c62b4e99543e8b6bb03ee73f47a7229a
MagdalenaSarul <magdalena.sarul@gmail.com>
2021-06-09 21:19:22 +0200

    Add files via upload
    init 1

---
+++ github-test/src/com/kuchnia/tomo/madz/Obiad.java
@@ -0,0 +1,10 @@
+package com.kuchnia.tomo.madz;
+
+public class Obiad {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Co dzić na obiad?");
+        System.out.println("Menu:");
+    }
+}
